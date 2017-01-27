dc0c2e6d351c529f3699055ba2094bfd816d63c5
susanaly <lai.susana@gmail.com>
2017-01-27 15:14:14 -0500

    first commit

---
+++ app_photo.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 25 15:17:41 2017
+
+@author: susanalaiyuen
+"""
+import os
+from flask import Flask, render_template, request, flash, redirect, url_for, send_from_directory
+from werkzeug import secure_filename
+
+UPLOAD_FOLDER = '/tmp/'
+ALLOWED_EXTENSIONS = set(['jpg', 'jpeg', 'gif', 'png'])
+
+# Initialize application
+app = Flask(__name__)
+
+# path to the upload directory
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Define allowed extensions for the file to be uploaded
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload')
+def main():
+  return render_template('upload.html')
+
+@app.route('/show/<filename>')
+def uploaded_file(filename):
+    filename = 'http://127.0.0.1:5000/tmp/' + filename
+    return render_template('template.html', filename=filename)
+
+@app.route('/tmp/<filename>')
+def send_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+    
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['file']  
+        # If user does not select a file
+        if f.filename == '':
+            #flash('No file selected')
+            return render_template('upload.html')
+            #return redirect(url_for('upload_file'))
+            #return redirect(request.url)
+        # If a file is selected that satisfies the extensions allowed,
+        # display the image
+        if f and allowed_file(f.filename):
+            filename = secure_filename(f.filename)
+            f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file', filename=filename))
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ tests/test_app_photo.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 26 15:15:17 2017
+
+@author: susanalaiyuen
+"""
+
+from flask import Flask, Request, request
+from StringIO import StringIO
+import unittest
+
+RESULT = False
+
+class TestFileFail(unittest.TestCase):
+    def test_1(self):
+        class FileObj(StringIO):
+            def close(self):
+                print 'in file close'
+                global RESULT
+                RESULT = True
+        
+        class MyRequest(Request):
+            def _get_file_stream(*args, **kwargs):
+                return FileObj()
+        
+        app = Flask(__name__)
+        
+        app.debug = True
+        app.request_class = MyRequest
+        
+        @app.route("/uploader", methods=['POST'])
+        def upload_file():
+            f = request.files['file']
+            print 'in upload handler'
+            self.assertIsInstance(
+                f.stream,
+                FileObj,
+                )
+            f.close()
+            return 'ok'
+        
+        client = app.test_client()
+        resp = client.post(
+            '/uploader',
+            data = {'file': (StringIO('my file contents'), 'test.jpg'),})
+        self.assertEqual('ok', resp.data,)
+        global RESULT
+        self.assertTrue(RESULT)
+
+
+if __name__ == '__main__':
+    unittest.main()
